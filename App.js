681a99254008a0b22b0306848c82846fa56c5e0d
Sarah Razanatsoa <sarah.raz@onja.org>
2020-11-19 15:27:28 +0300

    add the pages file needed

---
+++ App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
+import Home from './pages/Home'
+import Style from './pages/Style'
+import Add from './pages/Add'
+import Cart from './pages/Cart'
+
+function App() {
+    return (
+        <div>
+            <header>
+                <nav>
+                    <Link to = '/'>Popular Songs</Link>
+                    <Link to = '/styles'>Styles</Link>
+                    <Link to = '/add'>Add</Link>
+                    <Link to = '/cart'>Cart</Link>
+                </nav>
+            </header>
+            <Switch>
+                <Route exact path = '/'>
+                   <Home />
+                </Route>
+                <Route path = '/styles'>
+                    <Style />
+                </Route>
+                <Route path = '/add'>
+                    <Add />
+                </Route>
+                <Route path = '/cart'>
+                    <Cart />
+                </Route>
+            </Switch>
+        </div>
+    )
+}
+
+export default App
---
+++ pages/Style.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Styles() {
+    return (
+        <div>
+            <h1>Style Page</h1>
+        </div>
+    )
+}
+
+export default Styles
